1bf7ab86e9349fcc95b46b6be2158ab598b7a2bc
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2022-04-10 20:00:32 +0900

    Mo

---
+++ src/main/java/org/zerock/realmovie/RealMovieApplication.java
@@ -0,0 +1,16 @@
+package org.zerock.realmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealMovieApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RealMovieApplication.class, args);
+
+
+    }
+
+}
